4d0859efc82a7f5f733aa0518310ef002c835382
Alejandro Zorita <alejandro.zorita@medssocial.com>
2020-04-11 01:24:17 +0200

    construct

--- app/HtmlElement.php
+++ app/HtmlElement.php
@@ -2,11 +2,37 @@
 
 namespace App;
 
+/**
+ * Class HtmlElement
+ *
+ * @package App
+ */
 class HtmlElement
 {
+    /**
+     * @var \App\string
+     */
+    private $name;
 
+
+    /**
+     * HtmlElement constructor.
+     *
+     * @param  \App\string  $name
+     */
+    private function __construct(string $name)
+    {
+
+        $this->name = $name;
+    }
+
+
+    /**
+     * @return string
+     */
     public function render()
     {
         return "HTML RENDER";
     }
+
 }
