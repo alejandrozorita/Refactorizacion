9134199bb8c4f85e13d0ea49754016add91db2dd
Alejandro Zorita <alejandro.zorita@medssocial.com>
2020-04-11 20:04:48 +0200

    Antes refact

--- public/index.php
+++ public/index.php
@@ -3,9 +3,7 @@
 require '../vendor/autoload.php';
 
 $elements = new \App\HtmlElement('p', [], 'Este es el conteido');
-
 echo htmlentities($elements->render(), ENT_QUOTES, 'UTF-8');
-echo $elements->render();
 
 $elements = new \App\HtmlElement('p', [ 'id' => 'idElement' ], 'Este es el conteido');
 echo htmlentities($elements->render(), ENT_QUOTES, 'UTF-8');
@@ -22,10 +20,12 @@ echo htmlentities($elements->render(), ENT_QUOTES, 'UTF-8');
 
 echo "<br><hr>";
 
-$elements = new \App\HtmlElement('img', [ 'id' => 'idElement', 'src' => 'img.jpg', 'title' => 'Curso de "Refactorización" de alejandro'], 'Este es el conteido');
+$elements = new \App\HtmlElement('img', [
+  'id' => 'idElement', 'src' => 'img.jpg', 'title' => 'Curso de "Refactorización" de alejandro'
+], 'Este es el conteido');
 echo htmlentities($elements->render(), ENT_QUOTES, 'UTF-8');
 
 echo "<br><hr>";
 
-$elements = new \App\HtmlElement('input', ['required']);
+$elements = new \App\HtmlElement('input', [ 'required' ]);
 echo htmlentities($elements->render(), ENT_QUOTES, 'UTF-8');
--- tests/HtmlElementTest.php
+++ tests/HtmlElementTest.php
@@ -2,7 +2,98 @@
 
 namespace Tests;
 
+use App\HtmlElement;
+
 class HtmlElementTest extends TestCase
 {
+
+    /**
+     * @test
+     */
+    function it_generates_a_paragraph_whit_content()
+    {
+        $elements = new HtmlElement('p', [], 'Este es el conteido');
+
+        $this->assertSame(
+          '<p>Este es el conteido</p>',
+          $elements->render()
+        );
+    }
+
+
+    /**
+     * @test
+     */
+    function it_generates_a_paragraph_whit_content_and_id_attribute()
+    {
+        $elements = new HtmlElement('p', [ 'id' => 'idElement' ], 'Este es el conteido');
+
+        $this->assertSame(
+          '<p id="idElement">Este es el conteido</p>',
+          $elements->render()
+        );
+    }
+
+
+    /**
+     * @test
+     */
+    function it_generates_a_paragraph_whit_multiple_attributes()
+    {
+        $elements = new HtmlElement(
+          'p', [ 'id' => 'idElement', 'class' => 'succces' ], 'Este es el conteido'
+        );
+
+        $this->assertSame(
+          '<p id="idElement" class="succces">Este es el conteido</p>',
+          $elements->render()
+        );
+    }
+
+
+    /**
+     * @test
+     */
+    function it_generates_an_img_tag()
+    {
+        $elements = new HtmlElement(
+          'img', [ 'src' => 'img.jpg' ]
+        );
+
+        $this->assertSame(
+          '<img src="img.jpg">',
+          $elements->render()
+        );
+    }
+
+    /**
+     * @test
+     */
+    function it_escpoes_the_html_attributes()
+    {
+        $elements = new HtmlElement(
+          'img', [ 'src' => 'img.jpg', 'title' => 'Curso de "Refactorización" de alejandro']
+        );
+
+        $this->assertSame(
+          '<img src="img.jpg" title="Curso de &quot;Refactorizaci&oacute;n&quot; de alejandro">',
+          $elements->render()
+        );
+    }
+
+    /**
+     * @test
+     */
+    function it_generate_elements_with_boolean_attributes()
+    {
+        $elements = new HtmlElement(
+          'input', [ 'required' ]
+        );
+
+        $this->assertSame(
+          '<input required>',
+          $elements->render()
+        );
+    }
 
 }
