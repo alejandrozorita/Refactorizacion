46c785897191b5adde3e67fda806af50f99cb88d
Alejandro Zorita <alejandro.zorita@medssocial.com>
2020-04-11 02:34:48 +0200

    creando elemento para refacotrizar

--- app/HtmlElement.php
+++ app/HtmlElement.php
@@ -12,18 +12,24 @@ class HtmlElement
      * @var null
      */
     private $content;
+    /**
+     * @var array
+     */
+    private $attributes;
 
 
     /**
      * HtmlElement constructor.
      *
-     * @param  \App\string  $name
+     * @param  string  $name
+     * @param  array  $attributes
      * @param $content
      */
-    public function __construct(string $name, $content = null)
+    public function __construct(string $name, array $attributes = [], $content = NULL)
     {
         $this->name = $name;
         $this->content = $content;
+        $this->attributes = $attributes;
     }
 
 
@@ -32,7 +38,27 @@ class HtmlElement
      */
     public function render()
     {
-        $result = "<$this->name>";
+        // Si el elemento trie atributos
+        if ( ! empty($this->attributes) ) {
+
+            $htmlAttributes = '';
+
+            foreach ( $this->attributes as $name => $attribute ) {
+                $htmlAttributes .= " $name='$attribute'";
+            }
+            // Abrir la etiqueta con atributos
+            $result = "<$this->name$htmlAttributes>";
+        } else {
+            // Abrir la etiqeuta sin atributos
+            $result = "<$this->name>";
+        }
+
+        // Si es void
+        if(in_array($this->name, ['br','hr','img', 'input', 'meta'])) {
+            return $result;
+        }
+
+        // Retornar el resultado de una  vez
 
         $result .= $this->content;
 
--- public/index.php
+++ public/index.php
@@ -2,8 +2,25 @@
 
 require '../vendor/autoload.php';
 
-$elements = new \App\HtmlElement('p','Este es el conteido');
+$elements = new \App\HtmlElement('p', [], 'Este es el conteido');
 
-echo html_entity_decode($elements->render(), ENT_QUOTES, 'UTF-8');
+echo htmlentities($elements->render(), ENT_QUOTES, 'UTF-8');
 echo $elements->render();
 
+$elements = new \App\HtmlElement('p', [ 'id' => 'idElement' ], 'Este es el conteido');
+echo htmlentities($elements->render(), ENT_QUOTES, 'UTF-8');
+
+echo "<br><hr>";
+
+$elements = new \App\HtmlElement('p', [ 'id' => 'idElement', 'class' => 'succces' ], 'Este es el conteido');
+echo htmlentities($elements->render(), ENT_QUOTES, 'UTF-8');
+
+echo "<br><hr>";
+
+$elements = new \App\HtmlElement('img', [ 'id' => 'idElement', 'src' => 'img.jpg' ], 'Este es el conteido');
+echo htmlentities($elements->render(), ENT_QUOTES, 'UTF-8');
+
+echo "<br><hr>";
+
+$elements = new \App\HtmlElement('input');
+echo htmlentities($elements->render(), ENT_QUOTES, 'UTF-8');
